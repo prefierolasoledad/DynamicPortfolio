d9312d6dcd11eb88cbbb430e08fdc910b5431404
prefierolasoledad <karanbhatt9900@gmail.com>
2025-02-09 10:41:02 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-  basePath: '/dynamicPortfolio',
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
