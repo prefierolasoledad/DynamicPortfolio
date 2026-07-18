34b282f05b7ae79fbd081e5905c077373ee30cf5
prefierolasoledad <karanbhatt9900@gmail.com>
2026-07-18 17:42:56 +0530

    feat: add project data and implement About section details with GitHub stats

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -28,7 +28,7 @@ const AboutDetails = () => {
           className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
         >
           <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
-            10+ <sub className="font-semibold text-base">projects</sub>
+            8+ <sub className="font-semibold text-base">projects</sub>
           </p>
         </ItemLayout>
 
